dbffe995ec89d20387df2a594021f266c2b706ea
Edmundo Ogaz Baltierra <ext_eogazb@falabella.cl>
2023-05-02 11:39:43 -0400

    test

--- designPatterns/prototype-turing/src/main/es/labturing/connect4/test/Connect4.java
+++ designPatterns/prototype-turing/src/main/es/labturing/connect4/test/Connect4.java
@@ -5,7 +5,9 @@ import main.es.labturing.connect4.models.Turn;
 import main.es.labturing.connect4.models.Board;
 import main.es.labturing.connect4.models.RandomPlayer;
 import main.es.labturing.connect4.models.MachinePlayer;
+import main.es.labturing.connect4.models.MinMaxPlayer;
 import main.es.labturing.connect4.types.Coordinate;
+import main.es.labturing.connect4.types.Color;
 
 public class Connect4 {
 
@@ -13,34 +15,45 @@ public class Connect4 {
         Game game = new Game();
         Turn turn = game.getTurn();
         Board board = game.getBoard();
-        turn.addPlayer(new RandomPlayer(turn.getBoard()));
-        turn.addPlayer(new RandomPlayer(turn.getBoard()));
-        do {
-            MachinePlayer player = (MachinePlayer) turn.getActivePlayer();
-            turn.play(player.getColumn());
-        } while (board.isGameFinished());
-        showBoard(board);
-        System.out.println("fin");
+        
+        configTurnMinMax(turn);
+        play(turn, board);
+        showColors(board.getColors());
+        System.out.println("fin 1");
+
+        game.reset();
+        
+        configTurnRandom(turn);
+        play(turn, board);
+        showColors(board.getColors());
+        System.out.println("fin 2");
     }
 
-    private static void showBoard(Board board) {
-        for (int i = Coordinate.NUMBER_ROWS - 1; i >= 0; i--) {
-            System.out.print("|");
+    private static void configTurnRandom(Turn turn) {
+        turn.addPlayer(new RandomPlayer(turn.getBoard()));
+        turn.addPlayer(new RandomPlayer(turn.getBoard()));
+    }
 
+    private static void configTurnMinMax(Turn turn) {
+        turn.addPlayer(new MinMaxPlayer(turn.getBoard()));
+        turn.addPlayer(new RandomPlayer(turn.getBoard()));
+    }
+
+    private static void play(Turn turn, Board board) {
+        do {
+            MachinePlayer player = (MachinePlayer) turn.getActivePlayer();
+            int column = player.getColumn();
+            turn.play(column);
+        } while (!board.isGameFinished());
+        System.out.println("Coordinate" + board.getLastDrop().getColumn() );
+    }
+
+    private static void showColors(Color[][] colors) {
+        for (int i = Coordinate.NUMBER_ROWS - 1; i >= 0; i--) {
             for (int j = 0; j < Coordinate.NUMBER_COLUMNS; j++) {
-                board.getColor(new Coordinate(i, j)).name().charAt(0);
-                System.out.print("|");
+                System.out.print("|" + colors[i][j].name().charAt(0));
             }
             System.out.println(" ");
         }
-        writeHorizontal();
     }
-
-    private static void writeHorizontal() {
-        for (int i = 0; i < 4 * Coordinate.NUMBER_COLUMNS; i++) {
-            System.out.print("-");
-        }
-        System.out.print("-");
-    }
-
 }
